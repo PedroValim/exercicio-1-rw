3ca5e4e458be014ae94cf8e46243d9664b60ded2
PedroValim <pedrohlvalim@gmail.com>
2026-01-17 14:56:55 -0300

    exercicio 2 RW

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,28 +2,35 @@ describe('Login', () => {
 
   beforeEach(() => {
     cy.fixture('user').as('userData');
+    cy.visit('/signin');
   });
 
   it('Login com sucesso', function () {
-    cy.login(
-      this.userData.validUser.username,
-      this.userData.validUser.password
-    );
+    cy.get('[data-test="signin-username"]')
+      .type(this.userData.validUser.username);
 
-    cy.url().should('include', '/');
+    cy.get('[data-test="signin-password"]')
+      .type(this.userData.validUser.password);
+
+    cy.get('[data-test="signin-submit"]')
+      .click();
+
+    cy.url().should('not.include', '/signin');
     cy.get('[data-test="sidenav-username"]').should('be.visible');
   });
 
   it('Tentar fazer login com credenciais inválidas', function () {
-    cy.login(
-      this.userData.invalidUser.username,
-      this.userData.invalidUser.password
-    );
+    cy.get('[data-test="signin-username"]')
+      .type(this.userData.invalidUser.username);
 
-    cy.get('[data-test="signin-error"]').should(
-      'contain',
-      'Username or password is invalid'
-    );
+    cy.get('[data-test="signin-password"]')
+      .type(this.userData.invalidUser.password);
+
+    cy.get('[data-test="signin-submit"]')
+      .click();
+
+    cy.get('[data-test="signin-error"]')
+      .should('contain', 'Username or password is invalid');
   });
 
 });
---
+++ cypress/e2e/send-money.cy.js
@@ -0,0 +1,69 @@
+describe('Enviar Dinheiro', () => {
+
+  beforeEach(() => {
+    cy.fixture('user').as('userData');
+
+    // Login explícito (igual ao padrão do projeto exemplo)
+    cy.visit('/signin');
+
+    cy.get('[data-test="signin-username"]')
+      .type('cypress_user');
+
+    cy.get('[data-test="signin-password"]')
+      .type('123456');
+
+    cy.get('[data-test="signin-submit"]')
+      .click();
+
+    cy.url().should('not.include', '/signin');
+  });
+
+  it('Enviar dinheiro com saldo suficiente', function () {
+    cy.get('[data-test="nav-top-new-transaction"]')
+      .click();
+
+    cy.get('[data-test="user-list-search-input"]')
+      .type(this.userData.transaction.receiver);
+
+    cy.get('[data-test="user-list-item"]')
+      .first()
+      .click();
+
+    cy.get('[data-test="transaction-create-amount-input"]')
+      .type(this.userData.transaction.amountValid);
+
+    cy.get('[data-test="transaction-create-description-input"]')
+      .type(this.userData.transaction.description);
+
+    cy.get('[data-test="transaction-create-submit-payment"]')
+      .click();
+
+    cy.get('[data-test="alert-bar-success"]')
+      .should('be.visible');
+  });
+
+  it('Enviar dinheiro com saldo insuficiente', function () {
+    cy.get('[data-test="nav-top-new-transaction"]')
+      .click();
+
+    cy.get('[data-test="user-list-search-input"]')
+      .type(this.userData.transaction.receiver);
+
+    cy.get('[data-test="user-list-item"]')
+      .first()
+      .click();
+
+    cy.get('[data-test="transaction-create-amount-input"]')
+      .type(this.userData.transaction.amountInvalid);
+
+    cy.get('[data-test="transaction-create-description-input"]')
+      .type(this.userData.transaction.description);
+
+    cy.get('[data-test="transaction-create-submit-payment"]')
+      .click();
+
+    cy.get('[data-test="transaction-create-error"]')
+      .should('be.visible');
+  });
+
+});
